08ce348eb354e84df5124a56caa28f4cd5b5a389
= <vedank.pande@gmail.com>
2020-11-22 18:56:10 +0530

    data_process.py now imports from data_sort.py

--- data_process.py
+++ data_process.py
@@ -1,32 +1,40 @@
-import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torchvision
+from PIL import Image
+import data_sort
 
 TRAIN_PATH = '/home/vedank/Desktop/code/facial_sentiment/dataset/train.csv'
 TEST_PATH = '/home/vedank/Desktop/code/facial_sentiment/dataset/test.csv'
+ROOT = '/home/vedank/Desktop/code/facial_sentiment/data'
 
+# class names to name folders
+class_names = ['Angry','Disgust','Fear','Happy','Sad','Surprise','Neutral']
 
-
-def csv_to_np(PATH,dataset_type):
+#converts csv data to numpy arrays
+def csv_to_np(PATH,ROOT_PATH,dataset_type):
     a = []
     data = pd.read_csv(PATH)
     if dataset_type == "train":
         data,data_y = data.iloc[:,1:],data.iloc[:,:1]
-        np.save("/home/vedank/Desktop/code/facial_sentiment/npy_data/train_data_labels.npy",np.array(data_y))
+        np.save(f"{ROOT_PATH}/train_data_labels.npy",np.array(data_y))
     data = pd.Series(np.array(data).flatten())
     for i in range(len(data)):  
         img_str = data[i].split(' ')
         image = np.asarray(img_str,dtype=np.uint8).reshape(48,48,1)
         a.append(image)
 
-    data = np.array(a).squeeze(axis=3)
+    data = np.array(a,dtype=np.uint8).squeeze(axis=3)
 
-    data_tensor = torch.Tensor([np.repeat(image[...,np.newaxis],3,-1)/255.0 for image in data])
-    np.save(f"/home/vedank/Desktop/code/facial_sentiment/npy_data/{dataset_type}_data.npy",np.array(data_tensor))
+    data = np.array([np.repeat(image[...,np.newaxis],3,-1)/255.0 for image in data],dtype=np.float32)
+    return data 
 
 
-csv_to_np(TRAIN_PATH,"train")
-csv_to_np(TEST_PATH,"test")
+
+if __name__ == "__main__":
+    np_train = csv_to_np(TRAIN_PATH,ROOT,"train")
+    label = np.load(f"{ROOT}/train_data_labels.npy")
+    data_sort.numpy_to_jpeg_sorted(np_train,label,ROOT)
+
--- data_sort.py
+++ data_sort.py
@@ -1,29 +1,25 @@
-import os
 import numpy as np
 from PIL import Image
 
 # 0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral
 
-x_train = np.load('/home/vedank/Desktop/code/facial_sentiment/npy_data/train_data.npy')
-labels = np.load('/home/vedank/Desktop/code/facial_sentiment/npy_data/train_data_labels.npy')
-test =  np.load('/home/vedank/Desktop/code/facial_sentiment/npy_data/test_data.npy')
+# takes numpy arrays and saves them in jpeg format, in class based folders
+def numpy_to_jpeg_sorted(train,labels,ROOT_PATH):
 
-ROOT_PATH = '/home/vedank/Desktop/code/facial_sentiment/data'
-
-class_names = ['Angry','Disgust','Fear','Happy','Sad','Surprise','Neutral']
-
-class_count = {emote:0 for emote in class_names}
-
-for idx,image in enumerate(x_train):
-    emote = labels[idx]
-    emote_name = class_names[int(emote)]
-    class_count[emote_name] += 1
+    class_names = ['Angry','Disgust','Fear','Happy','Sad','Surprise','Neutral']
     
-    # saving in jpeg format so the pytorch dataloader.ImageFolder can read the files
-    im = Image.fromarray((image*255.0).astype(np.uint8))
-    im.save(f"{ROOT_PATH}/{class_names[int(emote)]}/{class_count[emote_name]}.jpeg")
+    class_count = {emote:0 for emote in class_names}
 
-    #uncomment below if you want .npy files instead
-    #np.save(f"{ROOT_PATH}/{class_names[int(emote)]}/{class_count[emote_name]}.npy",np.array(image))
+    for idx,image in enumerate(train):
+        emote = labels[idx]
+        emote_name = class_names[int(emote)]
+        class_count[emote_name] += 1
+        
+        # saving in jpeg format so the pytorch dataloader.ImageFolder can read the files
+        im = Image.fromarray((image*255.0).astype(np.uint8))
+        im.save(f"{ROOT_PATH}/{class_names[int(emote)]}/{class_count[emote_name]}.jpeg")
 
-print(class_count)
+        #uncomment below if you want .npy files instead
+        #np.save(f"{ROOT_PATH}/{class_names[int(emote)]}/{class_count[emote_name]}.npy",np.array(image))
+
+    print(class_count)
